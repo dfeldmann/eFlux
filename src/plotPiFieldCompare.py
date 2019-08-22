884412c3ff7233ba86ff3ade4a1c7565b055adf0
Daniel Feldmann <daniel.feldmann@zarm.uni-bremen.de>
2019-08-22 17:24:56 +0200

    Add script to compare pre-computed eFlux fields based on different kernels in a wall-parallel plane in one plot.

--- src/plotPiFieldCompare.py
+++ src/plotPiFieldCompare.py
@@ -67,6 +67,7 @@ if plot not in [1, 2]: sys.exit() # skip everything below
 print('Creating plot (using LaTeX)...')
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 mpl.rcParams['text.usetex'] = True
 mpl.rcParams['text.latex.preamble'] = [
@@ -97,7 +98,7 @@ def mm2inch(*tupl):
   return tuple(i/inch for i in tupl[0])
  else:
    return tuple(i/inch for i in tupl)
-fig = plt.figure(num=None, figsize=mm2inch(110.0, 51.0), dpi=300)
+fig = plt.figure(num=None, figsize=mm2inch(110.0, 65.0), dpi=300)
 
 # conservative colour palette appropriate for colour-blind (http://mkweb.bcgsc.ca/colorblind/)
 Vermillion    = '#D55E00'
@@ -126,45 +127,53 @@ clm   =-0.0020                        # manual contour level
 # modify box for filter name annotation
 filterBox = dict(boxstyle="square, pad=0.3", fc='w', ec=Black, lw=0.5)
 
+# axes grid for multiple subplots with common colour bar
+from mpl_toolkits.axes_grid1 import ImageGrid
+ig = ImageGrid(fig, 111, nrows_ncols=(3, 1), axes_pad=0.04, cbar_size=0.10, cbar_mode='single', cbar_location='right', cbar_pad=0.05)
+ax1 = ig[0]
+ax2 = ig[1]
+ax3 = ig[2]
+
 # plot Fourier eFlux
-ax1 = plt.subplot2grid((3, 15), (0, 0), rowspan=1, colspan=14) #, sharex=ax1)
-ax1.set_xticks([])
 ax1.set_ylabel(r"$\theta r$ in $R$")
 im1 = ax1.imshow(piF, vmin=-ampi, vmax=+ampi, cmap=VermBlue, interpolation='bilinear', extent=[np.min(z), np.max(z), np.min(th*r[k]), np.max(th*r[k])], origin='lower')
-cl1 = ax1.contour(z, th*r[k], piF, levels=[clm], colors=Black, linestyles='-', linewidths=0.1)
+#cl1 = ax1.contour(z, th*r[k], piF, levels=[clm], colors=Black, linestyles='-', linewidths=0.1)
 ax1.set_aspect('equal')
 t1 = ax1.text(1.0, 5.0, r"Fourier", ha="left", va="top", rotation=0, bbox=filterBox)
 
 # plot Gauss eFlux
-ax2 = plt.subplot2grid((3, 15), (1, 0), rowspan=1, colspan=14) #, sharex=ax1)
-ax2.set_xticks([])
 ax2.set_ylabel(r"$\theta r$ in $R$")
 im2 = ax2.imshow(piG, vmin=-ampi, vmax=+ampi, cmap=VermBlue, interpolation='bilinear', extent=[np.min(z), np.max(z), np.min(th*r[k]), np.max(th*r[k])], origin='lower')
-cl2 = ax2.contour(z, th*r[k], piG, levels=[clm], colors=Black, linestyles='-', linewidths=0.1)
+#cl2 = ax2.contour(z, th*r[k], piG, levels=[clm], colors=Black, linestyles='-', linewidths=0.1)
 ax2.set_aspect('equal')
 t2 = ax2.text(1.0, 5.0, r"Gauss", ha="left", va="top", rotation=0, bbox=filterBox)
 
 # plot box eFlux
-ax3 = plt.subplot2grid((3, 15), (2, 0), rowspan=1, colspan=14) #, sharex=ax1)
 ax3.set_xlabel(r"$z$ in $R$")
 ax3.set_ylabel(r"$\theta r$ in $R$")
 im3 = ax3.imshow(piB, vmin=-ampi, vmax=+ampi, cmap=VermBlue, interpolation='bilinear', extent=[np.min(z), np.max(z), np.min(th*r[k]), np.max(th*r[k])], origin='lower')
-cl3 = ax3.contour(z, th*r[k], piB, levels=[clm], colors=Black, linestyles='-', linewidths=0.1)
+#cl3 = ax3.contour(z, th*r[k], piB, levels=[clm], colors=Black, linestyles='-', linewidths=0.1)
 ax3.set_aspect('equal')
 t3 = ax3.text(1.0, 5.0, r"Box", ha="left", va="top", rotation=0, bbox=filterBox)
 
 # plot common colour bar
-ax4 = plt.subplot2grid((3, 15), (0, 14), rowspan=3, colspan=1) #, sharex=ax1)
-cb4 = plt.colorbar(im1, cax=ax4, orientation='vertical')
-cb4.set_ticks([-0.01, 0.0, +0.01])
-cb4.set_label(r"$\Pi$ in $U^{3}_{c} R^2$")
+fmt = FormatStrFormatter('%6.3f')
+cb1 = ax1.cax.colorbar(im1, format=fmt)
+cb1 = ig.cbar_axes[0].colorbar(im1)
+cb1.ax.set_ylabel(r"$\Pi$ in $U^{3}_{c}R^{2}$")
+cb1.ax.set_yticks([-ampi, 0.000, +ampi])
+#cb1.set_ticks([-ampi, clm, 0.000, -clm, +ampi]) # tweak colour bar ticks to show manual countour level
+
+# optional rectangle to highlight inset or zoom area
+from matplotlib.patches import Rectangle
+ax1.add_patch(Rectangle((0.0/Re_tau, 0.0/Re_tau), 1800.0/Re_tau, 600.0/Re_tau, fill=False, color=Vermillion, alpha=1, linewidth=2.0, clip_on=False, zorder=100))
 
 # plot mode interactive or pdf
 if plot != 2:
- plt.tight_layout()
+ #plt.tight_layout()
  plt.show()
 else:
- fig.tight_layout()
+ #fig.tight_layout()
  fnam = str.replace(fnam, '.h5', '.pdf')
  fnam = str.replace(fnam, 'piFieldBox2d', 'plotPiFieldCompare')
  plt.savefig(fnam)
