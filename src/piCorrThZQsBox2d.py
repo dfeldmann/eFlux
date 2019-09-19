c8ddcf9f3ca4582b57ca2baa478fad00f1b9ef46
umair-zarm <mohd_umair@yahoo.com>
2019-09-19 14:10:42 +0200

    Add script to compute eFlux and 2d two-point correlation maps in a wall-parallel (theta-z) plane for eFlux and Q events based on box filter.

--- src/piCorrThZQsBox2d.py
+++ src/piCorrThZQsBox2d.py
@@ -30,7 +30,7 @@ import numpy as np
 import h5py
 
 # range of state files to read flow field data
-iFirst =  1675000
+iFirst =  1675000 # 570000
 iLast  =  1675000
 iStep  =     5000
 iFiles = range(iFirst, iLast+iStep, iStep)
