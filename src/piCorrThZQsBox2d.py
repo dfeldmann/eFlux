d4c158ce2bfbe956a442b440469fd2c7c40489f6
umair-zarm <mohd_umair@yahoo.com>
2019-09-19 15:14:57 +0200

    Change index of first snapshot file.

--- src/piCorrThZQsBox2d.py
+++ src/piCorrThZQsBox2d.py
@@ -28,7 +28,7 @@ import numpy as np
 import h5py
 
 # range of state files to read flow field data (do modify)
-iFirst =  1675000 # 570000
+iFirst =   570000
 iLast  =  1675000
 iStep  =     5000
 iFiles = range(iFirst, iLast+iStep, iStep)
