38704fc07fb3c963b8cea3fd5d0d444431bf1cff
Daniel Feldmann <daniel.feldmann@zarm.uni-bremen.de>
2019-09-26 20:08:47 +0200

    Correct typo

--- src/plotPiStatCompareViscous.py
+++ src/plotPiStatCompareViscous.py
@@ -4,7 +4,7 @@
 #           reference data from literature (Hartel et la. 1994 PoF). Plot radial
 #           profiles of mean, RMS, skewness and flatness factors in viscous
 #           (inner) units. Output is interactive or as pfd figure file.
-# Usage:    python plotPiStatComapreViscous.py 
+# Usage:    python plotPiStatCompareViscous.py 
 # Authors:  Daniel Feldmann, Mohammad Umair
 # Date:     28th March 2019
 # Modified: 25th August 2019
@@ -22,7 +22,8 @@ Re_b   = 5300.0 # Bulk Reynolds number  Re_b   = u_b   * D / nu = u_cHP * R / nu
 Re_tau =  180.4 # Shear Reynolds number Re_tau = u_tau * R / nu
 
 # read one-point statistics for eflux Fourier filtered from ascii file
-fnam = 'piStatBox2d_pipe0002_01675000to01675000nt0001.dat' 
+fnam = 'piStatFourier2d_pipe0002_01675000to01675000nt0001.dat'
+fnam = 'piStatFourier2d_pipe0002_00570000to01675000nt0222.dat' 
 print('Reading eFlux statistics from', fnam)
 eFMeanFourier = np.loadtxt(fnam)[:, 1]
 eFRmsFourier  = np.loadtxt(fnam)[:, 2]
@@ -31,6 +32,7 @@ eFFlatFourier = np.loadtxt(fnam)[:, 4]
 
 # read one-point statistics for eflux Gauss filtered from ascii file
 fnam = 'piStatGauss2d_pipe0002_01675000to01675000nt0001.dat'
+fnam = 'piStatGauss2d_pipe0002_00570000to01675000nt0222.dat'
 print('Reading eFlux statistics from', fnam)
 eFMeanGauss = np.loadtxt(fnam)[:, 1]
 eFRmsGauss  = np.loadtxt(fnam)[:, 2]
@@ -39,6 +41,7 @@ eFFlatGauss = np.loadtxt(fnam)[:, 4]
 
 # read one-point statistics for eflux box filtered from ascii file
 fnam = 'piStatBox2d_pipe0002_01675000to01675000nt0001.dat'
+fnam = 'piStatBox2d_pipe0002_00570000to01675000nt0222.dat'
 print('Reading eFlux statistics from', fnam)
 r = np.loadtxt(fnam)[:, 0] # radial grid only once
 eFMeanBox = np.loadtxt(fnam)[:, 1]
@@ -123,12 +126,12 @@ ax1 = plt.subplot2grid((2, 2), (0, 0), rowspan=1, colspan=1)
 ax1.set_xscale('log')
 ax1.set_xlim(left=6.0e-1, right=Re_tau)
 ax1.set_xticks([])
-ax1.set_ylabel(r"$\langle \Pi^{\prime} \rangle_{\theta, z, t}$ in $ $")
+ax1.set_ylabel(r"$\langle\Pi\rangle_{\theta, z, t}$ in $U^3_{\text{HP}}R^2$")
 #ax1.set_ylim(bottom=-0.010, top=0.035)
 ax1.axhline(y=0.0, color=Grey)
-ax1.plot(yp[:-3], eFMeanBox[:-3]*fpi,     color=Blue,        linestyle='-',  zorder=9, label=r"Box")
-ax1.plot(yp[:-3], eFMeanGauss[:-3]*fpi,   color=Vermillion,  linestyle='-',  zorder=8, label=r"Gauss")
-ax1.plot(yp[:-3], eFMeanFourier[:-3]*fpi, color=Black,       linestyle='-',  zorder=7, label=r"Fourier")
+ax1.plot(yp[:-2], eFMeanFourier[:-2]*fpi, color=Black,      linestyle='-',  zorder=7, label=r"Fourier")
+ax1.plot(yp[:-2],   eFMeanGauss[:-2]*fpi, color=Vermillion, linestyle='-',  zorder=9, label=r"Gauss")
+ax1.plot(yp[:-2],     eFMeanBox[:-2]*fpi, color=Blue,       linestyle='-',  zorder=8, label=r"Box")
 #ax1.plot(ypRef[:], eFMeanRef[:],          color=BluishGreen, linestyle='-',  zorder=6, label=r"H\"artel et al. 1994")
 ax1.legend(loc='best', frameon=False, fancybox=False, facecolor=None, edgecolor=None, framealpha=None)
 
@@ -137,33 +140,33 @@ ax2 = plt.subplot2grid((2, 2), (0, 1), rowspan=1, colspan=1)
 ax2.set_xscale('log')
 ax2.set_xlim(left=6.0e-1, right=Re_tau)
 ax2.set_xticks([])
-ax2.set_ylabel(r"$\text{RMS}\left(\Pi^{\prime}\right)$ in $ $")
+ax2.set_ylabel(r"$\sqrt{ \langle\Pi^{\prime 2} \rangle_{\theta, z, t} }$ in $U^3_{\text{HP}}R^2$")
 #ax2.set_ylim(bottom=0.0, top=0.25)
-ax2.plot(yp[:-3], eFRmsBox[:-3]*fpi,     color=Blue,        linestyle='-',  label=r"Box")
-ax2.plot(yp[:-3], eFRmsGauss[:-3]*fpi,   color=Vermillion,  linestyle='-',  label=r"Gauss")
-ax2.plot(yp[:-3], eFRmsFourier[:-3]*fpi, color=Black,       linestyle='-',  label=r"Fourier")
+ax2.plot(yp[:-2], eFRmsFourier[:-2]*fpi, color=Black,      linestyle='-',  zorder=7, label=r"Fourier")
+ax2.plot(yp[:-2],   eFRmsGauss[:-2]*fpi, color=Vermillion, linestyle='-',  zorder=9, label=r"Gauss")
+ax2.plot(yp[:-2],     eFRmsBox[:-2]*fpi, color=Blue,       linestyle='-',  zorder=8, label=r"Box")
 
 # plot third order statistics
 ax3 = plt.subplot2grid((2, 2), (1, 0), rowspan=1, colspan=1)
 ax3.set_xlabel(r"$y^{+}$ in $\sfrac{\nu}{u_{\tau}}$")
 ax3.set_xscale('log')
 ax3.set_xlim(left=6.0e-1, right=Re_tau)
-ax3.set_ylabel(r"$\text{Skewness}\left(\Pi^{\prime}\right)$ in $\text{RMS}^{3}$")
+ax3.set_ylabel(r"$\langle\Pi^{\prime 3} \rangle_{\theta, z, t}$ in $\langle\Pi^{\prime 2} \rangle^{\sfrac{3}{2}}_{\theta, z, t} $  ")
 ax3.axhline(y=0.0, color=Grey)
-ax3.plot(yp[:-1], eFSkewBox[:-1],     color=Blue,        linestyle='-',  zorder=9, label=r"Box")
-ax3.plot(yp[:-1], eFSkewGauss[:-1],   color=Vermillion,  linestyle='-',  zorder=8, label=r"Gauss")
-ax3.plot(yp[:-1], eFSkewFourier[:-1], color=Black, linestyle='-',  zorder=7, label=r"Fourier")
+ax3.plot(yp[:-2], eFSkewFourier[:-2]*fpi, color=Black,      linestyle='-',  zorder=7, label=r"Fourier")
+ax3.plot(yp[:-2],   eFSkewGauss[:-2]*fpi, color=Vermillion, linestyle='-',  zorder=9, label=r"Gauss")
+ax3.plot(yp[:-2],     eFSkewBox[:-2]*fpi, color=Blue,       linestyle='-',  zorder=8, label=r"Box")
 
 # plot fourth order statistics
 ax4 = plt.subplot2grid((2, 2), (1, 1), rowspan=1, colspan=1)
 ax4.set_xlabel(r"$y^{+}$ in $\sfrac{\nu}{u_{\tau}}$")
 ax4.set_xscale('log')
 ax4.set_xlim(left=6.0e-1, right=Re_tau)
-ax4.set_ylabel(r"$\text{Flatness}\left(\Pi^{\prime}\right)$ in $\text{RMS}^{4}$")
+ax4.set_ylabel(r"$\langle\Pi^{\prime 4} \rangle_{\theta, z, t}$ in $\langle\Pi^{\prime 2} \rangle^{\sfrac{4}{2}}_{\theta, z, t} $  ")
 ax4.set_ylim(bottom=0.0, top=500.0)
-ax4.plot(yp[:-1], eFFlatBox[:-1],     color=Blue,        linestyle='-',  zorder=9, label=r"Box")
-ax4.plot(yp[:-1], eFFlatGauss[:-1],   color=Vermillion,  linestyle='-',  zorder=8, label=r"Gauss")
-ax4.plot(yp[:-1], eFFlatFourier[:-1], color=Black,       linestyle='-',  zorder=7, label=r"Fourier")
+ax4.plot(yp[:-2], eFFlatFourier[:-2]*fpi, color=Black,      linestyle='-',  zorder=7, label=r"Fourier")
+ax4.plot(yp[:-2],   eFFlatGauss[:-2]*fpi, color=Vermillion, linestyle='-',  zorder=9, label=r"Gauss")
+ax4.plot(yp[:-2],     eFFlatBox[:-2]*fpi, color=Blue,       linestyle='-',  zorder=8, label=r"Box")
 
 # plot mode interactive or pdf
 if plot == 1:
