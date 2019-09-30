93de1c027cd3b35dd6baca64a330875a8af098b9
Daniel Feldmann <daniel.feldmann@zarm.uni-bremen.de>
2019-09-30 14:39:07 +0200

    Add example pdf figure and minor updates.

--- src/plotPiFieldCompare.py
+++ src/plotPiFieldCompare.py
@@ -2,17 +2,15 @@
 # Purpose:  Read pre-computed instantaneous 3d energy flux fields from HDF5 file
 #           based on different filter kernels (Fourier, Gauss, box). Extract 2d
 #           data sets in a wall-parallel plane and plot them to compare the
-#           effect of the filter kernel. Additional contour lines can be plotted
-#           on top. Output is interactive or as pfd figure file.
+#           effect of the filter kernel. Additional contour lines and a box to
+#           to highlight a zoom region can be plotted on top. Output is
+#           interactive or as pfd figure file.
 # Usage:    python plotPiFieldCompare.py 
 # Authors:  Daniel Feldmann, Mohammad Umair
 # Date:     28th March 2019
-# Modified: 21st August 2019
+# Modified: 30th September 2019
 
-import sys
-import os.path
 import timeit
-import math
 import numpy as np
 import h5py
 
@@ -62,34 +60,35 @@ print ("Extracting 2d data in wall-parallel plane at y+ =", (1-r[k])*Re_tau)
 piF = piF[k, :, :]  
 piG = piG[k, :, :]  
 piB = piB[k, :, :]  
-    
+
+# plotting
 if plot not in [1, 2]: sys.exit() # skip everything below
 print('Creating plot (using LaTeX)...')
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FormatStrFormatter
-from mpl_toolkits.axes_grid1 import make_axes_locatable
 mpl.rcParams['text.usetex'] = True
 mpl.rcParams['text.latex.preamble'] = [
-r"\usepackage[utf8x]{inputenc}",
+r"\usepackage[utf8]{inputenc}",
 r"\usepackage[T1]{fontenc}",
+r'\usepackage{lmodern, palatino, eulervm}',
+#r'\usepackage{mathptmx}',
 r"\usepackage[detect-all]{siunitx}",
 r'\usepackage{amsmath, amstext, amssymb}',
-r'\usepackage{xfrac}',
-r'\usepackage{lmodern, palatino, eulervm}']
-mpl.rcParams.update({'font.family': 'sans-serif'})
-mpl.rcParams.update({'font.family': 'serif'})
-mpl.rcParams.update({'font.size': 5})
-mpl.rcParams.update({'lines.linewidth': 0.5})
-mpl.rcParams.update({'axes.linewidth': 0.5})
-mpl.rcParams['xtick.major.size'] = 2
-mpl.rcParams['xtick.major.width'] = 0.5
-mpl.rcParams['xtick.minor.size'] = 1
-mpl.rcParams['xtick.minor.width'] = 0.5
-mpl.rcParams['ytick.major.size'] = 2
-mpl.rcParams['ytick.major.width'] = 0.5
-mpl.rcParams['ytick.minor.size'] = 1
-mpl.rcParams['ytick.minor.width'] = 0.5
+r'\usepackage{xfrac}']
+#mpl.rcParams.update({'font.family': 'sans-serif'})
+mpl.rcParams.update({'font.family' : 'serif'})
+mpl.rcParams.update({'font.size' : 8})
+mpl.rcParams.update({'lines.linewidth'   : 0.75})
+mpl.rcParams.update({'axes.linewidth'    : 0.75})
+mpl.rcParams.update({'xtick.major.size'  : 2.00})
+mpl.rcParams.update({'xtick.major.width' : 0.75})
+mpl.rcParams.update({'xtick.minor.size'  : 1.00})
+mpl.rcParams.update({'xtick.minor.width' : 0.75})
+mpl.rcParams.update({'ytick.major.size'  : 2.00})
+mpl.rcParams.update({'ytick.major.width' : 0.75})
+mpl.rcParams.update({'ytick.minor.size'  : 1.00})
+mpl.rcParams.update({'ytick.minor.width' : 0.75})
 
 # create figure suitable for A4 format
 def mm2inch(*tupl):
@@ -98,7 +97,8 @@ def mm2inch(*tupl):
   return tuple(i/inch for i in tupl[0])
  else:
    return tuple(i/inch for i in tupl)
-fig = plt.figure(num=None, figsize=mm2inch(110.0, 65.0), dpi=300)
+#fig = plt.figure(num=None, figsize=mm2inch(135.0, 65.0), dpi=300)
+fig = plt.figure(num=None, dpi=300)
 
 # conservative colour palette appropriate for colour-blind (http://mkweb.bcgsc.ca/colorblind/)
 Vermillion    = '#D55E00'
@@ -139,14 +139,16 @@ ax1.set_ylabel(r"$\theta r$ in $R$")
 im1 = ax1.imshow(piF, vmin=-ampi, vmax=+ampi, cmap=VermBlue, interpolation='bilinear', extent=[np.min(z), np.max(z), np.min(th*r[k]), np.max(th*r[k])], origin='lower')
 #cl1 = ax1.contour(z, th*r[k], piF, levels=[clm], colors=Black, linestyles='-', linewidths=0.1)
 ax1.set_aspect('equal')
-t1 = ax1.text(1.0, 5.0, r"Fourier", ha="left", va="top", rotation=0, bbox=filterBox)
+ax1.text(0.985, 0.9, r"Fourier", ha="right", va="top", transform=ax1.transAxes, rotation=0, bbox=filterBox)
+ax1.text(0.012, 0.9, r"a)", ha="left", va="top", transform=ax1.transAxes)
 
 # plot Gauss eFlux
 ax2.set_ylabel(r"$\theta r$ in $R$")
 im2 = ax2.imshow(piG, vmin=-ampi, vmax=+ampi, cmap=VermBlue, interpolation='bilinear', extent=[np.min(z), np.max(z), np.min(th*r[k]), np.max(th*r[k])], origin='lower')
 #cl2 = ax2.contour(z, th*r[k], piG, levels=[clm], colors=Black, linestyles='-', linewidths=0.1)
 ax2.set_aspect('equal')
-t2 = ax2.text(1.0, 5.0, r"Gauss", ha="left", va="top", rotation=0, bbox=filterBox)
+ax2.text(0.985, 0.9, r"Gauss", ha="right", va="top", transform=ax2.transAxes, rotation=0, bbox=filterBox)
+ax2.text(0.012, 0.9, r"b)", ha="left", va="top", transform=ax2.transAxes)
 
 # plot box eFlux
 ax3.set_xlabel(r"$z$ in $R$")
@@ -154,10 +156,11 @@ ax3.set_ylabel(r"$\theta r$ in $R$")
 im3 = ax3.imshow(piB, vmin=-ampi, vmax=+ampi, cmap=VermBlue, interpolation='bilinear', extent=[np.min(z), np.max(z), np.min(th*r[k]), np.max(th*r[k])], origin='lower')
 #cl3 = ax3.contour(z, th*r[k], piB, levels=[clm], colors=Black, linestyles='-', linewidths=0.1)
 ax3.set_aspect('equal')
-t3 = ax3.text(1.0, 5.0, r"Box", ha="left", va="top", rotation=0, bbox=filterBox)
+ax3.text(0.985, 0.9, r"Box", ha="right", va="top", transform=ax3.transAxes, rotation=0, bbox=filterBox)
+ax3.text(0.012, 0.9, r"c)", ha="left", va="top", transform=ax3.transAxes)
 
 # plot common colour bar
-fmt = FormatStrFormatter('%6.3f')
+fmt = FormatStrFormatter('%6.2f')
 cb1 = ax1.cax.colorbar(im1, format=fmt)
 cb1 = ig.cbar_axes[0].colorbar(im1)
 cb1.ax.set_ylabel(r"$\Pi$ in $U^{3}_{c}R^{2}$")
@@ -166,7 +169,9 @@ cb1.ax.set_yticks([-ampi, 0.000, +ampi])
 
 # optional rectangle to highlight inset or zoom area
 from matplotlib.patches import Rectangle
-ax1.add_patch(Rectangle((0.0/Re_tau, 0.0/Re_tau), 1800.0/Re_tau, 600.0/Re_tau, fill=False, color=Vermillion, alpha=1, linewidth=2.0, clip_on=False, zorder=100))
+ax1.add_patch(Rectangle((0.0/Re_tau, 0.0/Re_tau), 1800.0/Re_tau, 600.0/Re_tau, fill=False, color=Vermillion, alpha=1, linewidth=1.0, clip_on=False, zorder=100))
+ax2.add_patch(Rectangle((0.0/Re_tau, 0.0/Re_tau), 1800.0/Re_tau, 600.0/Re_tau, fill=False, color=Vermillion, alpha=1, linewidth=1.0, clip_on=False, zorder=100))
+ax3.add_patch(Rectangle((0.0/Re_tau, 0.0/Re_tau), 1800.0/Re_tau, 600.0/Re_tau, fill=False, color=Vermillion, alpha=1, linewidth=1.0, clip_on=False, zorder=100))
 
 # plot mode interactive or pdf
 if plot != 2:
