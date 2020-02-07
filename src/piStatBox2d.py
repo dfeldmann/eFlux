2cae056003e878a4aadb2b2c9b3c075075cf7c38
umair-zarm <mohd_umair@yahoo.com>
2020-02-07 14:12:18 +0100

    Update input file range

--- src/piStatBox2d.py
+++ src/piStatBox2d.py
@@ -27,8 +27,8 @@ plot = 0
 
 # range of state files to read from flow field data
 iFirst =  570000
-iLast  = 1675000
-iStep  =    5000
+iLast  = 4070000 # 1675000
+iStep  =   10000 #    5000
 iFiles = range(iFirst, iLast+iStep, iStep)
 print('Compute energy flux one-point statistics from', len(iFiles), 'snapshot(s):', iFiles[0], 'to', iFiles[-1])
 
